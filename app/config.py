3020edd81737c23ba162516a8ce6f9c5a9a4b39e
keenno11 <118120069+keenno11@users.noreply.github.com>
2026-05-13 13:47:40 +0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 class Config:
     SECRET_KEY = 'super_secret_key'
-    SQLALCHEMY_DATABASE_URI = ('postgresql://postgres:root@localhost/korochki_est')
+    SQLALCHEMY_DATABASE_URI = ('postgresql://kval_db_user:CIGqGKcAIAHHNjgI7ILVEVFAfyjuiVEe@dpg-d824fmjtqb8s73c8gui0-a/kval_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
